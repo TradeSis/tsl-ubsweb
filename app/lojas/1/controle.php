7146b9e9527620f752556009cb9fd0b35b2ccecf
TradeSis Helio <helio@tradesis.com.br>
2024-08-21 16:49:50 -0300

    fix helio

--- app/lojas/1/controle.php
+++ app/lojas/1/controle.php
@@ -12,7 +12,7 @@ $explode = explode("/",$funcao);
 if ($explode[0]) {
   $funcao = $explode[0];
 }
-if ($explode[1]) {
+if (isset($explode[1])) {
   $parametro2 = $explode[1];
 }
 /*if ($explode[2]) {
