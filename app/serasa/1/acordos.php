82391ea1e596198ef67defcf0969ec5ae161975d
tradesishelio <99908450+tradesishelio@users.noreply.github.com>
2024-09-25 16:59:51 -0300

    serasa - ajustes na api

--- app/serasa/1/acordos.php
+++ app/serasa/1/acordos.php
@@ -13,7 +13,7 @@ function isJson($string)
 }
 
 fwrite($arquivo, $identificacao . "-ENTRADA->" . json_encode($jsonEntrada) . "\n");
-//fwrite($arquivo, $identificacao . "-PARAMETRO->" . json_encode($parametro) . "\n");
+fwrite($arquivo, $identificacao . "-PARAMETRO->" . json_encode($parametro) . "\n");
 
 
  $conteudoEntrada = json_encode(array(
@@ -27,52 +27,56 @@ fwrite($arquivo, $identificacao . "-CONTEUDO->" . json_encode($conteudoEntrada) 
 
 $retorno = $progr->executarprogress("serasa/1/acordos", $conteudoEntrada, $dlc, $pf, $propath, $progresscfg, $tmp, $proginicial);
 fwrite($arquivo, $identificacao . "-RETORNO->" . json_encode($retorno) . "\n");
+$dados = json_decode($retorno,true);
+  if (isset($dados["conteudoSaida"][0])) { // Conteudo Saida - Caso de erro
+      $dados = $dados["conteudoSaida"][0];
+      $jsonSaida = $dados;
+  } else {
+              $conteudoSaida  = (object) json_decode($retorno,true);
+              $acordos        = $conteudoSaida->JSON["acordos"][0];
+              $instalments    = $conteudoSaida->JSON["instalments"]; 
+              $taxes          = $conteudoSaida->JSON["taxes"][0]; 
 
-$conteudoSaida  = (object) json_decode($retorno,true);
-$acordos        = $conteudoSaida->JSON["acordos"][0];
-$instalments    = $conteudoSaida->JSON["instalments"]; 
-$taxes          = $conteudoSaida->JSON["taxes"][0]; 
+              //fwrite($arquivo, $identificacao . "-CONTEUDO SAIDA->" . json_encode($conteudoSaida) . "\n");
+              //fwrite($arquivo, $identificacao . "-ACORDOS->" . json_encode($acordos) . "\n");
+              //fwrite($arquivo, $identificacao . "-INSTALMENTS->" . json_encode($instalments) . "\n");
+              //fwrite($arquivo, $identificacao . "-TAXES->" . json_encode($taxes) . "\n");
 
-//fwrite($arquivo, $identificacao . "-CONTEUDO SAIDA->" . json_encode($conteudoSaida) . "\n");
-//fwrite($arquivo, $identificacao . "-ACORDOS->" . json_encode($acordos) . "\n");
-//fwrite($arquivo, $identificacao . "-INSTALMENTS->" . json_encode($instalments) . "\n");
-//fwrite($arquivo, $identificacao . "-TAXES->" . json_encode($taxes) . "\n");
+              $iof = array(
+                "percentage" => $taxes["iof_percentage"], //0
+                "totalValue" => $taxes["iof_totalValue"], //0
+              );
 
-$iof = array(
-  "percentage" => $taxes["iof_percentage"], //0
-  "totalValue" => $taxes["iof_totalValue"], //0
-);
+              $cet = array(
+                "yearPercentage" => $taxes["cet_yearPercentage"], //0,
+                "monthPercentage" => $taxes["cet_monthPercentage"], //0,
+                "totalValue" => $taxes["cet_totalValue"] //0
+              );
 
-$cet = array(
-  "yearPercentage" => $taxes["cet_yearPercentage"], //0,
-  "monthPercentage" => $taxes["cet_monthPercentage"], //0,
-  "totalValue" => $taxes["cet_totalValue"] //0
-);
+              $interest = array(
+                "yearPercentage" => $taxes["interest_yearPercentage"], //0,
+                "monthPercentage" => $taxes["interest_monthPercentage"], //0,
+                "totalValue" => $taxes["interest_totalValue"] //0
+              );
 
-$interest = array(
-  "yearPercentage" => $taxes["interest_yearPercentage"], //0,
-  "monthPercentage" => $taxes["interest_monthPercentage"], //0,
-  "totalValue" => $taxes["interest_totalValue"] //0
-);
+              $taxes = array(
+                "iof" => $iof,
+                "cet" => $cet,
+                "interest" => $interest
+              );
 
-$taxes = array(
-  "iof" => $iof,
-  "cet" => $cet,
-  "interest" => $interest
-);
+              $acordo = array(
+                "offerId" => $acordos["offerId"], //"8d4b3cc7-5020-4c57-aa76-52eb9f28ab2a",
+                "agreementId" => $acordos["agreementId"], //"123456",
+                "total" => $acordos["total"], //300,
+                "totalWithoutInterest" => $acordos["totalWithoutInterest"], //300,
+                "discountValue" => $acordos["discountValue"], //100,
+                "discountPercentage" => $acordos["discountPercentage"], //25,
+                "instalments" => $instalments,
+                "taxes" => $taxes,
+              );
 
-$acordo = array(
-  "offerId" => $acordos["offerId"], //"8d4b3cc7-5020-4c57-aa76-52eb9f28ab2a",
-  "agreementId" => $acordos["agreementId"], //"123456",
-  "total" => $acordos["total"], //300,
-  "totalWithoutInterest" => $acordos["totalWithoutInterest"], //300,
-  "discountValue" => $acordos["discountValue"], //100,
-  "discountPercentage" => $acordos["discountPercentage"], //25,
-  "instalments" => $instalments,
-  "taxes" => $taxes,
-);
-
-$jsonSaida = $acordo;
-
+              $jsonSaida = $acordo;
+            }
 
 fclose($arquivo);
